badbd2b09b7ff5ff042b8a574a90de72e5769319
Francisco José Cháves <pachopepe@gmail.com>
2015-09-24 00:09:05 -0500

    Producer-Consumer

--- spring-jms-consumer-example/src/main/java/edu/eci/test/events/Main.java
+++ spring-jms-consumer-example/src/main/java/edu/eci/test/events/Main.java
@@ -1,9 +1,5 @@
 package edu.eci.test.events;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.jms.JMSException;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,13 +8,6 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		MyJMSProducer mp=ac.getBean(MyJMSProducer.class);
-		
-            try {                
-                    mp.sendMessages(new String("hola mundo!"));
-            } catch (JMSException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
 	}
 	
 	
--- spring-jms-producer-example/src/main/java/edu/eci/test/events/Main.java
+++ spring-jms-producer-example/src/main/java/edu/eci/test/events/Main.java
@@ -1,5 +1,9 @@
 package edu.eci.test.events;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.JMSException;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,6 +12,13 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
 		
+		MyJMSProducer mp=ac.getBean(MyJMSProducer.class);
+		
+            try {                
+                    mp.sendMessages(new String("hola mundo!"));
+            } catch (JMSException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
 	}
 	
 	
